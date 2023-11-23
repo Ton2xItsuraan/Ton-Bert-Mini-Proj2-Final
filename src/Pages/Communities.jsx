6f2142ee987ae39ab7958c969196a16974f21be4
Peacewalker404 <joebertlazano.404@gmail.com>
2023-11-23 20:15:15 +0800

    a

--- src/Pages/Communities.jsx
+++ src/Pages/Communities.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import ProtectedPage from './ProtectedPage'
+import React from 'react';
+import ProtectedPage from './ProtectedPage';
+import { Box, Heading, Text, Button, Stack } from '@chakra-ui/react';
 
 function Communities() {
   return (
     <ProtectedPage>
-      Hi
+      <Box p={4} borderWidth="1px" borderRadius="lg">
+        <Heading size="xl" mb={4}>
+          Welcome to Our Communities!
+        </Heading>
+
+        <Stack spacing={4}>
+          <CommunityLink
+            name="Facebook"
+            description="The goal of this page is to unite communities on certain topics."
+            link="https://your-community-link-1"
+          />
+          <CommunityLink
+            name="Twitter"
+            description="Be Inform and updated."
+            link="https://your-community-link-2"
+          />
+          {/* Add more communities as needed */}
+        </Stack>
+      </Box>
     </ProtectedPage>
-  )
+  );
 }
 
-export default Communities
+// Custom component for each community link
+function CommunityLink({ name, description, link }) {
+  return (
+    <Box borderWidth="1px" p={4} borderRadius="md">
+      <Heading size="md">{name}</Heading>
+      <Text color="gray.500" mb={2}>
+        {description}
+      </Text>
+      <Button as="a" href={link} target="_blank" rel="noopener noreferrer" colorScheme="blue">
+        Join Community
+      </Button>
+    </Box>
+  );
+}
+
+export default Communities;
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import ProtectedPage from './ProtectedPage'
+import React from 'react';
+import ProtectedPage from './ProtectedPage';
+import { Box, Heading, Text, Button, Avatar, Stack } from '@chakra-ui/react';
 
 function Profile() {
   return (
     <ProtectedPage>
-      Hi
+      <Box p={4} borderWidth="1px" borderRadius="lg">
+        <Stack spacing={4} align="center">
+          <Avatar size="xl" name="Joebert Lazano" src="url-to-your-profile-image" />
+          <Heading size="lg" color="white">Joebert Lazano</Heading>
+          <Text fontSize="md" color="gray.500">
+            Former Kid
+          </Text>
+          <Text mb={8} fontSize="xl" fontWeight="bold" color="white">
+            Welcome to your awesome profile page!
+          </Text>
+          <Text mb={3} color="white">Nickname     :   Jon-Jon</Text>
+          <Text mb={3} color="white">Age          :   30</Text>
+          <Text mb={3} color="white">Birthplace   :   Metro Manila</Text>
+          <Text mb={3} color="white">Occupation   :   Metro Aid</Text>
+          <Text mb={3} color="white">"Someone who cannot abandon everything cannot achieve anything"</Text>
+
+          {/* Add your profile content here */}
+          <Button colorScheme="purple">Edit Profile</Button>
+
+          {/* Social Media Links */}
+          <Stack direction="row" spacing={4} mt={4}>
+            <Button as="a" href="your-link-to-facebook" target="_blank" rel="noopener noreferrer" colorScheme="facebook">
+              Facebook
+            </Button>
+            <Button as="a" href="your-link-to-twitter" target="_blank" rel="noopener noreferrer" colorScheme="twitter">
+              Twitter
+            </Button>
+            {/* Add more social media links as needed */}
+          </Stack>
+        </Stack>
+      </Box>
     </ProtectedPage>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
